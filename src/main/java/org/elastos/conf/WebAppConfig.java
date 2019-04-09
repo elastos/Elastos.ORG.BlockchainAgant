11d446166e6e76f65d9e681874f742a96a22578c
patternmap <patternmap@163.com>
2019-04-09 20:12:28 +0800

    1. Remove cross site access.

--- src/main/java/org/elastos/conf/WebAppConfig.java
+++ src/main/java/org/elastos/conf/WebAppConfig.java
@@ -3,16 +3,16 @@ package org.elastos.conf;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
-@Configuration
-@EnableWebMvc
-public class WebAppConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
-                .allowedMethods("*")
-                .maxAge(3600);
-    }
-}
+//@Configuration
+//@EnableWebMvc
+//public class WebAppConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowCredentials(true)
+//                .allowedMethods("*")
+//                .maxAge(3600);
+//    }
+//}
