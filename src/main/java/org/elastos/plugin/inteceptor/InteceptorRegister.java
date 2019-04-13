db736408c754a7dae6cdfddf7e89cae4cf893e8a
patternmap <patternmap@163.com>
2019-04-13 22:58:21 +0800

    1. Add cross site function. 2. Change get param method.

--- src/main/java/org/elastos/plugin/inteceptor/InteceptorRegister.java
+++ src/main/java/org/elastos/plugin/inteceptor/InteceptorRegister.java
@@ -7,19 +7,39 @@
 package org.elastos.plugin.inteceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class InteceptorRegister implements WebMvcConfigurer {
 
     @Autowired
-    private ApiInterceptor interceptor;
+    ApiInterceptor interceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor);
+        registry.addInterceptor(interceptor)
+                .excludePathPatterns("/error")
+                .excludePathPatterns("/static/*");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+
+
 }
